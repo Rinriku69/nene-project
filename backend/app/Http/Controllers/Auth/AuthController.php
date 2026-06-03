f3d8d5a63119fdb8af7b4c2bc199c17ccba6a97d
Rinriku <siripoohkim@gmail.com>
2026-06-03 18:34:54 +0700

    feat: user register

---
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    protected function register(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'username' => ['required', 'string', 'max:20', 'unique:users,username'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'] // หาfield password_confirmation ให้เอง
+        ]);
+
+        try {
+            $user = new User();
+            $user->email = $request['email'];
+            $user->username = $request['username'];
+            $user->password = $request['password'];
+            $user->role = "user";
+            $user->save();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Registered Successfully'
+            ], 200);
+        } catch (QueryException $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ], 400);
+        }
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,3 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
+
+//Authentication
+
+Route::controller(AuthController::class)
+    ->prefix('/auth')
+    ->name('auth.')
+    ->group(static function():void{
+        Route::post('register','register')->name('register');
+    });
