754ace8439f18ca0143b7d89e475c993ea5ac60c
Rinriku <siripoohkim@gmail.com>
2026-06-03 15:53:43 +0700

    feature(auth): register/login form validation + footer

--- frontend/src/app/nene-project/pages/auth/login/login.ts
+++ frontend/src/app/nene-project/pages/auth/login/login.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { form, FormField, required } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink,FormField],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  private readonly loginModel = signal({
+    username:'',
+    password:''
+  })
+
+  protected readonly loginForm = form(this.loginModel,(path)=>{
+    required(path.username,{message:'Username is required'});
+    required(path.password,{message:'Password is required'})
+  })
+
+}
--- frontend/src/app/nene-project/pages/auth/register/register.ts
+++ frontend/src/app/nene-project/pages/auth/register/register.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, computed, Signal, signal } from '@angular/core';
+import { form, FormField, minLength, required, validate } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, FormField],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  protected readonly formSubmitDisable: Signal<boolean> = computed(() => {
+    if (this.registerForm().invalid() || this.registerForm.password_confirmation().invalid()) {
+      return true
+    }
+    return false
+  });
+
+  private readonly registerModel = signal({
+    nickname: '',
+    username: '',
+    password: '',
+    password_confirmation: '',
+  });
+  protected readonly registerForm = form(this.registerModel, (path) => {
+    required(path.nickname, { message: 'Nickname is required' });
+    required(path.username, { message: 'Username is required' });
+    required(path.password, { message: 'Password is required' });
+    required(path.password_confirmation,{message:'Please confirm your password'})
+    validate(path.password_confirmation, ({ value }) => {
+      if (value() !== this.registerForm.password().value()) {
+        return { kind: 'https', message: 'Confirm password not match' };
+      }
+      return null;
+    });
+    minLength(path.password, 8, { message: 'Password must have atleast 8 characters' });
+  });
+}
