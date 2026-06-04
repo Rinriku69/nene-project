66075f908dee6faa5e1a35ed229e1bd03c058a8b
Rinriku <siripoohkim@gmail.com>
2026-06-04 22:42:32 +0700

    feat(auth): register service and component function

--- frontend/src/app/nene-project/pages/auth/register/register.ts
+++ frontend/src/app/nene-project/pages/auth/register/register.ts
@@ -1,7 +1,8 @@
-import { Component, computed, Signal, signal, WritableSignal } from '@angular/core';
+import { Component, computed, effect, inject, Signal, signal, WritableSignal } from '@angular/core';
 import { debounce, email, form, FormField, minLength, required, validate } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 import { RegisterModel } from '../../../models/AuthModel';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,6 +11,7 @@ import { RegisterModel } from '../../../models/AuthModel';
   styleUrl: './register.css',
 })
 export class Register {
+  private readonly authService = inject(AuthService);
   protected readonly formSubmitDisable: Signal<boolean> = computed(() => {
     if (this.registerForm().invalid() || this.registerForm.password_confirmation().invalid()) {
       return true
@@ -31,11 +33,41 @@ export class Register {
     required(path.password, { message: 'Password is required' });
     required(path.password_confirmation,{message:'Please confirm your password'})
     validate(path.password_confirmation, ({ value }) => {
-      if (value() !== this.registerForm.password().value()) {
+      if (value() !== null && value() !== this.registerForm.password().value()) {
         return { kind: 'https', message: 'Confirm password not match' };
       }
       return null;
     });
     minLength(path.password, 8, { message: 'Password must have atleast 8 characters' });
   });
+
+  /* constructor(){
+    effect(()=>{
+      console.log(this.registerForm.password_confirmation().errors())
+    })
+  } */
+
+  protected submit(){
+    if(this.registerForm().invalid()){
+      console.log("register form invalid")
+      return
+    }
+
+    this.authService.getCSRFToken().subscribe({
+      next:()=>{
+        this.authService.register(this.registerForm().value()).subscribe({
+          next: (response) => {
+            console.log(response)
+          },
+          error: (error) =>{
+            console.error(error.error.message)
+          }
+        })
+      },
+      error:(err)=>{
+        console.log("Failed to get CSRF Token")
+        console.log(err)
+      }
+    })
+  }
 }
--- frontend/src/app/nene-project/services/auth.service.ts
+++ frontend/src/app/nene-project/services/auth.service.ts
@@ -1,14 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { RegisterModel } from '../models/AuthModel';
+import { ResourceResponse } from '../models/Resource';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly http = inject(HttpClient);
+  private readonly baseApiUrl = "http://localhost:8000/api/auth";
 
-  protected readonly register(): Observable{
+  getCSRFToken():Observable<ResourceResponse>{
+    return this.http.get<ResourceResponse>("http://localhost:8000/sanctum/csrf-cookie");
+  }
 
+  register(registerFormData: RegisterModel):Observable<ResourceResponse>{
+    return this.http.post<ResourceResponse>(`${this.baseApiUrl}/register`,registerFormData)
   }
 }
