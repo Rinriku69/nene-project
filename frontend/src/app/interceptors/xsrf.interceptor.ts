fb149c40ac466c6df671412bd210b25d3cc5cde1
Rinriku <siripoohkim@gmail.com>
2026-06-04 22:41:16 +0700

    feat(auth): create interceptor add withCredential:true and X-XSRF-TOKEN on request

---
+++ frontend/src/app/interceptors/xsrf.interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpInterceptorFn } from "@angular/common/http";
+
+
+function getCookie(name:string):string| null{
+    const match = document.cookie.match(new RegExp(`(^|;\\s*)(${name})=([^;]*)`));
+    return match ? decodeURIComponent(match[3]) : null;
+}
+
+export const xsrfInterceptor: HttpInterceptorFn = (req,next) =>{
+    const xsrfToken = getCookie("XSRF-TOKEN");
+
+    let reqHeaders = req.headers;
+    if(xsrfToken){
+        reqHeaders = reqHeaders.set('X-XSRF-TOKEN',xsrfToken);
+        console.log("Token found")
+    }else{
+        console.log("Token Not Found in cookie")
+    }
+
+    const cloneReq = req.clone({
+        headers:reqHeaders,
+        withCredentials:true
+    })
+    // console.log(cloneReq.headers);
+    return next(cloneReq);
+}
