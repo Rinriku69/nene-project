2c4b1cf33c3906c2817d84be1c41b5a8dae37d88
Rinriku <siripoohkim@gmail.com>
2026-06-07 13:19:06 +0700

    feat: auth required dashboard route

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -11,7 +11,7 @@ export const authGuard: CanActivateFn = (route, state)=>{
         return true
     }
 
-    return router.parseUrl('/login')
+    return router.parseUrl('/auth/login')
 }
 
 export const roleGuard = (allowedRoles: string[]):CanActivateFn =>{
--- frontend/src/app/nene-project/routes.ts
+++ frontend/src/app/nene-project/routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from "@angular/router";
 import { Dashboard } from "./pages/app/dashboard/dashboard";
 import { MainLayout } from "./pages/app/main-layout/main-layout";
-import { roleGuard } from "../guards/auth.guard";
+import { authGuard, roleGuard } from "../guards/auth.guard";
 
 export default [
     {path:'',component:MainLayout,
         children:[
             {path:'',redirectTo:'dashboard',pathMatch:'full'},
-            {path:'dashboard',component:Dashboard},
+            {path:'dashboard',loadComponent:() => import('./pages/app/dashboard/dashboard').then(m=>m.Dashboard),
+                canMatch:[authGuard]
+            },
             // {path:'',loadComponent:() => import('./abc/abc.component').then(m=>m.AbcComponent),canMatch:[roleGuard(['admin'])]}
         ]
     },
