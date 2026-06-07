c3977187b37e5038347b9563ebdcb890eeb2f228
Rinriku <siripoohkim@gmail.com>
2026-06-07 20:50:24 +0700

    feat(db): items seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -17,9 +17,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
+       /*  User::factory()->create([
+            'username' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+ */
+        $this->call([
+            ItemSeeder::class,
         ]);
     }
 }
