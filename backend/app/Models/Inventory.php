dbf1a70832ab4603efa04a47778db764c28d7595
Rinriku <siripoohkim@gmail.com>
2026-06-08 16:28:28 +0700

    fix(model): change inventory relation hasone to belongsto

--- backend/app/Models/Inventory.php
+++ backend/app/Models/Inventory.php
@@ -4,17 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 #[Fillable(['user_id', 'item_id'])]
 class Inventory extends Model
 {
 
-    function items(): HasOne{
-        return $this->hasOne(Item::class);
+    function items(): BelongsTo{
+        return $this->belongsTo(Item::class);
     }
 
-    function users(): HasOne{
-        return $this->hasOne(User::class);
+    function users(): BelongsTo{
+        return $this->belongsTo(User::class);
     }
 }
