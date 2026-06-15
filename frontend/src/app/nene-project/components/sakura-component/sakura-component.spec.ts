13a550189177db5f07389fa4fd4e86dd8c9efe6b
Rinriku <siripoohkim@gmail.com>
2026-06-15 13:01:30 +0700

    feat: sakura component

---
+++ frontend/src/app/nene-project/components/sakura-component/sakura-component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SakuraComponent } from './sakura-component';
+
+describe('SakuraComponent', () => {
+  let component: SakuraComponent;
+  let fixture: ComponentFixture<SakuraComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SakuraComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SakuraComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
