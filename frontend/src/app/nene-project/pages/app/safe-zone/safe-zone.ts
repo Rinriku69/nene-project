5bf5766cc56f65bc4540142672743d2ba37e4c8e
Rinriku <siripoohkim@gmail.com>
2026-06-15 17:58:20 +0700

    feat: tanzaku ghost movement

--- frontend/src/app/nene-project/pages/app/safe-zone/safe-zone.ts
+++ frontend/src/app/nene-project/pages/app/safe-zone/safe-zone.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, ElementRef, signal, viewChild } from '@angular/core';
 import { SakuraComponent } from '../../../components/sakura-component/sakura-component';
+import { SafeZoneModel } from '../../../models/FormModel';
+import { form, required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-safe-zone',
@@ -9,6 +11,36 @@ import { SakuraComponent } from '../../../components/sakura-component/sakura-com
 })
 export class SafeZone {
 
-  modalIsOpen = signal<boolean>(false);
-  
+  sakuraImg = viewChild<ElementRef>('sakuraImg');
+ 
+  readonly modalIsOpen = signal<boolean>(false);
+  readonly isPlacementMode = signal<boolean>(false);
+
+  readonly ghostX = signal(20);
+  readonly ghostY = signal(0);
+
+  readonly messageModel = signal<SafeZoneModel>({
+    message:'',
+    unlocked_at:null
+  })
+
+  protected readonly messageForm = form(this.messageModel,(path)=>{
+    required(path.message);
+  })
+
+  updateGhostPosition(event: MouseEvent){
+    if(!this.isPlacementMode()) return
+    const rect:DOMRect  = this.sakuraImg()?.nativeElement.getBoundingClientRect();
+    const xPercent = ((event.clientX - rect.left) / rect.width) * 100;
+    const yPercent = ((event.clientY - rect.top) / rect.height) * 100;
+    this.ghostX.set(xPercent);
+    this.ghostY.set(yPercent);
+  }
+
+  togglePlacementMode(){
+    this.isPlacementMode.set(!this.isPlacementMode())
+  }
+
+
+
 }
