3060c9948cd8eb1856c36a03c9402088fb49cba2
Rinriku <siripoohkim@gmail.com>
2026-06-15 17:11:54 +0700

    style: sakura background

--- frontend/src/app/nene-project/pages/app/safe-zone/safe-zone.ts
+++ frontend/src/app/nene-project/pages/app/safe-zone/safe-zone.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { SakuraComponent } from '../../../components/sakura-component/sakura-component';
 
 @Component({
   selector: 'app-safe-zone',
-  imports: [],
+  imports: [SakuraComponent],
   templateUrl: './safe-zone.html',
   styleUrl: './safe-zone.css',
 })
-export class SafeZone {}
+export class SafeZone {
+
+  modalIsOpen = signal<boolean>(false);
+  
+}
