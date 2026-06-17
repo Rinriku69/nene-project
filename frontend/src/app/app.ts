85c9b41d184296c1c95937eb257fc689b1d4968c
Rinriku <siripoohkim@gmail.com>
2026-06-17 21:32:52 +0700

    fix: mobile ui

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { NavigationEnd, Router, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  private readonly router = inject(Router);
+  protected readonly isHidden = toSignal(
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      map(event => event.urlAfterRedirects.includes('/gacha'))
+    ),
+    { initialValue: this.router.url.includes('/gacha') }
+  );
 }
