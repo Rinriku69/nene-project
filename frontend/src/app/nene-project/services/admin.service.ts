e6c91c286a34fb51e3ffc8ee12b7c9f318d46317
Rinriku <siripoohkim@gmail.com>
2026-06-18 14:08:34 +0700

    chore: production url

--- frontend/src/app/nene-project/services/admin.service.ts
+++ frontend/src/app/nene-project/services/admin.service.ts
@@ -11,7 +11,7 @@ import { NotificationModel, UpdateUserForm } from '../models/FormModel';
   providedIn: 'root',
 })
 export class AdminService {
-  private readonly baseApiUrl = "http://localhost:8000/api/admin"
+  private readonly baseApiUrl = "/api/admin"
   private readonly http = inject(HttpClient);
 
   getUserList(term:filterTerm): Observable<PaginationResponse<UserResource>>{
