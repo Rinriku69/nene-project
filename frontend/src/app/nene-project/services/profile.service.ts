6a615d40231434171636d359f4a10dc9508cfd6e
Rinriku <siripoohkim@gmail.com>
2026-06-24 12:05:06 +0700

    refactor: production url

--- frontend/src/app/nene-project/services/profile.service.ts
+++ frontend/src/app/nene-project/services/profile.service.ts
@@ -6,7 +6,7 @@ import { ProfileImage, ResourceResponse } from '../models/Resource';
 @Service()
 export class ProfileService {
     private readonly http = inject(HttpClient);
-    private readonly baseApiUrl = 'http://localhost:8000/api/profile';
+    private readonly baseApiUrl = '/api/profile';
 
     uploadProfile(image: FormData):Observable<ResourceResponse>{
         return this.http.post<ResourceResponse>(`${this.baseApiUrl}/uploadProfile`,image)
