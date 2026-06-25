1da73b8f17a1fafb15bbeac616e2d29c74c4564d
Rinriku <siripoohkim@gmail.com>
2026-06-25 23:53:49 +0700

    feat: pet display + random animation

--- frontend/src/app/nene-project/helpers.ts
+++ frontend/src/app/nene-project/helpers.ts
@@ -2,3 +2,12 @@
 export function getRandomInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export async function getPetIdLocalStorage(key: string): Promise<number>{
+   return JSON.parse(localStorage.getItem(`${key}`) ?? 'null');
+}
+
+export function setPetIdLocalStorage(key:string, value: object | string | number): void{
+    return localStorage.setItem(`${key}`, JSON.stringify(value));
+}
+
