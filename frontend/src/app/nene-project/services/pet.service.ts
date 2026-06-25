4d54b20038ae648711d7aa0cd899e3c3a3b91783
Rinriku <siripoohkim@gmail.com>
2026-06-25 15:34:53 +0700

    feat: getUserPet

---
+++ frontend/src/app/nene-project/services/pet.service.ts
@@ -0,0 +1,14 @@
+import { computed, Service, signal } from '@angular/core';
+import { PetAnimation, UserPet } from '../models/Resource';
+import { httpResource, HttpResourceRef } from '@angular/common/http';
+
+@Service()
+export class PetService {
+    readonly petId = signal<number|null>(null);
+    private readonly petResource = httpResource<UserPet>(()=>({
+        url:`http://localhost:8000/api/pet/getUserPet/${this.petId()}`,
+        method: 'GET' 
+        })
+    )
+    readonly currentUserPet = computed(()=>this.petResource)
+}
