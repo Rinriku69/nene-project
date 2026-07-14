469af9c56eefcce65e6b2703b30d50ee9cff5811
Rinriku <siripoohkim@gmail.com>
2026-07-14 18:01:05 +0700

    feat: jwt login logout

---
+++ backend/app/Http/Controllers/Auth/JWTAuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\JWTException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenInvalidException;
+
+class JWTAuthController extends Controller
+{
+    function login(Request $request): JsonResponse
+    {
+        $credentail = $request->validate([
+            'username' => ['required', 'string', 'max:20'],
+            'password' => ['required', 'string', 'min:8']
+        ]);
+        try {
+            if (!$token = Auth::guard('api')->attempt($credentail)) {
+                return response()->json(['error' => 'Username or Password is incorrect'], 401);
+            }
+            return response()->json(['access_token' => $token], 200);
+        } catch (JWTException $e) {
+            return response()->json('error ' . $e->getMessage());
+        }
+    }
+
+    function logout(): JsonResponse{
+        Auth::guard('api')->logout();
+        return response()->json(['message'=>'Logged out successfully'],200);
+    }
+}
