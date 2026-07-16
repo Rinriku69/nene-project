6744afb99d465cb528858cc7f763d47e2bdb1131
Rinriku <siripoohkim@gmail.com>
2026-07-16 16:58:28 +0700

    fix: except stardew route from csrf

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -24,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->statefulApi();
         $middleware->preventRequestForgery(except: [
             'api/auth/jwt/*',
+            'api/stardew/*'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
