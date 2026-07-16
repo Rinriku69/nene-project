b8aa04b7e17ffcaa36b40df43af610172e1fec88
Rinriku <siripoohkim@gmail.com>
2026-07-16 22:52:11 +0700

    feat: gamesaves GET api

---
+++ backend/app/Http/Resources/StardewSaveResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StardewSaveResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'farm_name' => $this->farm_name,
+            'game_version' => $this->game_version,
+            'mod_version' => $this->mod_version,
+            'day' => $this->day,
+            'season' => $this->season,
+            'year' => $this->year,
+            'money' => $this->money,
+            'total_money_earned' => $this->total_money_earned,
+            'players'=>StardewPlayerResource::collection($this->players)
+        ];
+    }
+}
