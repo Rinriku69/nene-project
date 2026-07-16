a6e2104a99c5fd8d0c1e2c6413b736ad68b6cccb
Rinriku <siripoohkim@gmail.com>
2026-07-16 15:49:41 +0700

    chore: stardew models

---
+++ backend/app/Models/StardewSave.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class StardewSave extends Model
+{
+    function players(): HasMany{
+        return $this->hasMany(StardewPlayer::class);
+    }
+}
