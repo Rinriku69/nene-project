5ea223283b7bac20c41cb69f1c53d78e3de1f513
Rinriku <siripoohkim@gmail.com>
2026-07-19 18:09:40 +0700

    fix: add verify to api route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,9 +1,3 @@
 <?php
 
-use App\Http\Controllers\Auth\EmailVerificationController;
 use Illuminate\Support\Facades\Route;
-
-
-Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])
-    ->middleware(['signed'])
-    ->name('verification.verify');
