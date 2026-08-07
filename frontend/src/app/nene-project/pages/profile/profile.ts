51f17bdcd226c4386e486b0414dfd5fe441c0fff
Rinriku <siripoohkim@gmail.com>
2026-08-07 13:33:26 +0700

    fix: cant upload profile if not verify

--- frontend/src/app/nene-project/pages/profile/profile.ts
+++ frontend/src/app/nene-project/pages/profile/profile.ts
@@ -27,6 +27,10 @@ export class Profile {
   });
 
   onFileSelected(event: Event): void {
+    if(!this.authService.isVerified()){
+      this.authService.showBanner() ;
+      return
+    }
     const input = event.target as HTMLInputElement;
     const file = input.files?.[0] ?? null;
 
