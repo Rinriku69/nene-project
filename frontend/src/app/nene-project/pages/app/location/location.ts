f776d6cbcbb2c4b7c046ee88b582cda8e994706a
Rinriku <siripoohkim@gmail.com>
2026-08-20 11:50:29 +0700

    feat: update self position

--- frontend/src/app/nene-project/pages/app/location/location.ts
+++ frontend/src/app/nene-project/pages/app/location/location.ts
@@ -12,10 +12,11 @@ import {
 } from '@angular/core';
 import * as L from 'leaflet';
 import { LocationService } from '../../../services/location.service';
-import { MapPosition, PositionUpdate, ResourceErrorResponse } from '../../../models/Resource';
+import { LocationResource, MapPosition, PositionUpdate, ResourceErrorResponse } from '../../../models/Resource';
 import { StateService } from '../../../services/state.service';
+import { Icons } from '../../../components/icons/icons';
 
-const TIME_OUT = 5 * 1_000;
+const TIME_OUT = 10 * 1_000;
 const MAX_AGE = 0;
 const GET_POSITION_TIME = 5 * 1_000;
 
@@ -35,8 +36,21 @@ export class Location {
   private readonly myMarker = signal<L.Marker | null>(null);
   protected readonly isSharing = signal<boolean>(true);
   private readonly myPosition = signal<PositionUpdate | null>(null);
-  private readonly myPositionState = computed<PositionUpdate | null>(()=>this.myPosition());
+  private readonly myPositionState = computed<PositionUpdate | null>(() => this.myPosition());
   private getUserId?: number;
+
+  private readonly myPinIcon = L.divIcon({
+    className:'bg-blue-500 animate-pulse',
+    html:'<div></div>',
+    iconSize:[16,16],
+    iconAnchor:[8,8]
+  })
+  private readonly friendPinIcon = L.divIcon({
+    className:'bg-primary animate-pulse',
+    html:'<div></div>',
+    iconSize:[16,16],
+    iconAnchor:[8,8]
+  })
 
   protected readonly textStatus = signal<string | null>(null);
 
@@ -47,33 +61,38 @@ export class Location {
         const long = this.myLocation()?.long ?? -0.09;
         
       } */
-     const currentPosition = this.myPositionState();
-     const leafletMap = this.map();
-     if(currentPosition && leafletMap){
-      const lat = currentPosition.lat
-      const long = currentPosition.long
-      this.myMarker.set(L.marker([lat,long]).addTo(leafletMap))
-      leafletMap.setView([lat,long],leafletMap.getZoom(),{
-        animate:true
-      })
-     }
-    //  console.log('after render run')
+      const currentPosition = this.myPositionState();
+      const leafletMap = this.map();
+      if (currentPosition && leafletMap) {
+        const lat = currentPosition.lat;
+        const long = currentPosition.long;
+        const existingMarker = this.myMarker();
+
+        if (!existingMarker) {
+          leafletMap.setView([lat, long], leafletMap.getZoom(), { animate: true });
+          this.myMarker.set(L.marker([lat, long],{icon: this.myPinIcon}).addTo(leafletMap));
+        } else {
+          existingMarker.setLatLng([lat, long]);
+        }
+      }
+      /*  console.log('after render run')
+     console.log(this.myPositionState()) */
     });
 
     afterNextRender(() => {
-      if(!this.map()){ 
-          const leafletMap = L.map(this.mapContainer().nativeElement).setView([51.505, -0.09], 13);
-          this.map.set(leafletMap);
-          this.loadTileLayer(leafletMap);
-        }
+      if (!this.map()) {
+        const leafletMap = L.map(this.mapContainer().nativeElement).setView([51.505, -0.09], 13);
+        this.map.set(leafletMap);
+        this.loadTileLayer(leafletMap);
+      }
       this.updateUserMapPosition();
     });
 
-    this.destroyRef.onDestroy(()=>{
+    this.destroyRef.onDestroy(() => {
       this.map()?.remove();
       this.map.set(null);
-      clearTimeout(this.getUserId)
-    })
+      clearTimeout(this.getUserId);
+    });
   }
 
   loadTileLayer(map: L.Map): void {
@@ -89,9 +108,9 @@ export class Location {
 
   updatePosition(position: PositionUpdate): void {
     this.locationService.updatePosition(position).subscribe({
-      error: (err:ResourceErrorResponse)=>{
-        this.stateService.setErrorMessage(`Error ${err.error.message}`)
-      }
+      error: (err: ResourceErrorResponse) => {
+        this.stateService.setErrorMessage(`Error ${err.error.message}`);
+      },
     });
   }
 
@@ -111,7 +130,7 @@ export class Location {
       },
       (error) => {
         // console.error(`Error ${error.message}`)
-        this.stateService.setErrorMessage(`Error ${error.message}`)
+        this.stateService.setErrorMessage(`Error ${error.message}`);
       },
       {
         enableHighAccuracy: true,
@@ -120,6 +139,17 @@ export class Location {
       },
     );
     // console.log('updated')
-    this.getUserId = setTimeout(()=>this.updateUserMapPosition(),GET_POSITION_TIME);
+    this.getUserId = setTimeout(() => this.updateUserMapPosition(), GET_POSITION_TIME);
+  }
+
+  getFriendLocation(): void{
+    this.locationService.getFriendLocation().subscribe({
+      next:(res)=>{
+
+      },
+      error:(err)=>{
+
+      }
+    })
   }
 }
